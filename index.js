f8c1ef2d14a202003141060b09fceeb1a609e9c0
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-02-14 12:15:01 +0100

    Make it compatible with 0.7alpha

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ var Sealious = require("sealious");
 Sealious.init();
 
 new Sealious.ChipTypes.ResourceType({
-name: "person",
-fields: [
-{name: "name", type: "text", required: true},
-{name: "age", type: "int", required: true}
-]});
-
-Sealious.ChipManager.get_chip("channel", "rest").set_url_base("/api/v1");
+	name: "person",
+	fields: [
+		{name: "name", type: "text", required: false},
+		{name: "age", type: "int", required: true},
+		{name: "color", type: "color"}
+	],
+	access_strategy: {
+		"delete": "just_owner"
+	}
+});
 
 Sealious.start();
 
